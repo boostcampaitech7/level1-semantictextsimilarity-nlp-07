30139f9d8e4c75512e1353a3248873628cbeb34f
Seungbeom oh <osr0912@gmail.com>
2024-09-18 17:11:33 +0900

    refactor: config refactoring Path and parameters

---
+++ src/config/data_loader_config.py
@@ -0,0 +1,15 @@
+
+DATA_LOADER_CONFIG = { 
+  "batch_size" : 16,
+  "shuffle" : True,
+  "train_path" : './data/train_after_hanspell (1).csv',
+  "dev_path" : './data/dev_after_hanspell.csv',
+  "test_path" : './data/dev_after_hanspell.csv',
+  "predict_path" : './data/test_after_hanspell.csv',
+  "output_path" : './data/sample_submission.csv'
+}
+
+OPTIMIZER_CONFIG = {
+  "learning_rate": 1e-5,
+  "max_epoch": 50
+  }
