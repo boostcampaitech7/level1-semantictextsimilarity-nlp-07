c09da7dd8849af612e939120cbd31469d6ee44ac
gsgh3016 <gsgh3016@gmail.com>
2024-09-18 20:37:21 +0900

    feat: add experiment

---
+++ main.py
@@ -0,0 +1,48 @@
+from src.config.data_loader_config import DATA_LOADER_CONFIG, OPTIMIZER_CONFIG
+from src.data_loader.loader import Dataloader
+from src.model.model import Model, Models, LossFunctions
+from src.trainer.predict import save_result
+import src.callback as callback
+import pytorch_lightning as pl
+
+# Parameters 선언
+batch_size = DATA_LOADER_CONFIG['batch_size']
+shuffle = DATA_LOADER_CONFIG['shuffle']
+learning_rate = OPTIMIZER_CONFIG['learning_rate']
+max_epoch = OPTIMIZER_CONFIG['max_epoch']
+ 
+if __name__ == "__main__":
+    model_name = 'electra_base_v3'
+    model = Model(Models.electra_base_v3, learning_rate, LossFunctions.mse_loss)
+    print('Calling Model is Successful')
+    
+    # Dataloader 선언
+    dataloader = Dataloader(batch_size, shuffle, model_name=Models.electra_base_v3)
+    print('Calling Dataloader')
+    
+    
+    # callback 정의
+    epoch_print_callback = callback.EpochPrintCallback()
+    checkpoint_callback = callback.ModelCheckpoint(model_name=model_name)
+    early_stopping = callback.EarlyStopping()
+    lr_monitor = callback.LearningRateMonitor()
+    print('Calling Callback')
+    
+    # 학습 및 검증.
+    print('Start Training')
+    trainer = pl.Trainer(accelerator='gpu', devices=1, max_epochs=max_epoch, callbacks=[lr_monitor, epoch_print_callback,checkpoint_callback, early_stopping])
+    trainer.fit(model=model, datamodule=dataloader)
+    
+    print('------'*20)
+    trainer.test(model=model, datamodule=dataloader)
+    
+    # 가장 좋은 모델 불러오기
+    best_model_path = checkpoint_callback.best_model_path
+    model = Model.load_from_checkpoint(best_model_path)
+    print('----------'*10, 'Start Prediction', '----------'*10)
+    # 추론
+    predictions = trainer.predict(model=model, datamodule=dataloader)
+
+    # 결과 저장
+    save_result(predictions, model_name, max_epoch)
+    print('----------'*10, 'Finish', '----------'*10)
