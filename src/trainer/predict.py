65d74a6fa99f70a650be1263135c08fbbaec3cc0
Seungbeom oh <osr0912@gmail.com>
2024-09-18 17:17:23 +0900

    refactor: prediction and output

---
+++ src/trainer/predict.py
@@ -0,0 +1,17 @@
+from src.config.path_config import BASE, OUTPUT_PATH
+import pandas as pd
+import torch
+import pytorch_lightning as pl
+from pathlib import Path
+
+def save_result(data, model_name, max_epoch, filename=None):
+    data = list(round(float(i), 1) for i in torch.cat(data))
+    output = pd.read_csv(OUTPUT_PATH)
+    output['target'] = data
+    
+    if filename:
+        output_filename = filename
+    else:
+        output_filename = f'output_base_{max_epoch}_{model_name}_mse.csv'
+    SAVE_PATH = Path(BASE, output_filename)
+    output.to_csv(output_filename, index=False)
