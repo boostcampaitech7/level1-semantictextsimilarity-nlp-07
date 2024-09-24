9212ad302a28dc4a044aee1ee6b0f9def69505bb
Seungbeom oh <osr0912@gmail.com>
2024-09-24 21:14:12 +0900

    fix: predict.py

--- src/trainer/predict.py
+++ src/trainer/predict.py
@@ -5,19 +5,17 @@ import pytorch_lightning as pl
 
 from pathlib import Path
 
-def save_result(data, model_name, max_epoch, filename=None, mode ='output'):
+def save_result(data, model_name, max_epoch, mode ='output'):
     data = list(round(float(i), 1) for i in torch.cat(data))
     if mode =='output':
         output = pd.read_csv(OUTPUT_PATH)
         output['target'] = data
+        output_filename = f'output_{max_epoch}_{model_name}.csv'
         
     elif mode == 'train':
         output = pd.read_csv(TRAIN_PATH)
         output['target'] = data
-    
-    if filename:
-        output_filename = filename
-    else:
-        output_filename = f'output_base_{max_epoch}_{model_name}_mse.csv'
+        output_filename = f'train_{max_epoch}_{model_name}.csv'
+
     SAVE_PATH = Path(BASE, output_filename)
-    output.to_csv(SAVE_PATH, index=False)
+    output.to_csv(SAVE_PATH, index=False)   
